9acea590379338d1534d0e871604762355bf6b38
wangtongmeng <tongmengwang@163.com>
2020-04-09 00:03:50 +0800

    vuepress: 安装scss

--- docs/.vuepress/components/css/logo.vue
+++ docs/.vuepress/components/css/logo.vue
@@ -8,37 +8,37 @@
 export default {};
 </script>
 
-<style>
+<style lang="scss">
 .header-logo {
   display: inline-block;
   width: 55px;
   height: 55px;
   background-color: #ff6600;
-}
-.header-logo a {
-  display: inline-block;
-  width: 110px;
-  height: 55px;
-}
-.header-logo a:before {
-  content: " ";
-  display: inline-block;
-  width: 55px;
-  height: 55px;
-  background: url("./img/mi-logo.png") no-repeat center;
-  background-size: 55px;
-  transition: margin 0.2s;
-}
-.header-logo a:after {
-  content: " ";
-  display: inline-block;
-  width: 55px;
-  height: 55px;
-  background: url("./img/mi-home.png") no-repeat center;
-  background-size: 55px;
-}
-.header-logo a:hover:before {
-  margin-left: -55px;
-  transition: margin 0.2s;
+  a {
+    display: inline-block;
+    width: 110px;
+    height: 55px;
+    &:before {
+      content: " ";
+      display: inline-block;
+      width: 55px;
+      height: 55px;
+      background: url("./imgs/mi-logo.png") no-repeat center;
+      background-size: 55px;
+      transition: margin 0.2s;
+    }
+    &:after {
+      content: " ";
+      display: inline-block;
+      width: 55px;
+      height: 55px;
+      background: url("./imgs/mi-home.png") no-repeat center;
+      background-size: 55px;
+    }
+    &:hover:before {
+      margin-left: -55px;
+      transition: margin 0.2s;
+    }
+  }
 }
 </style>
