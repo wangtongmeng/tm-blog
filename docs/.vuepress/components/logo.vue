bcdc0b146b0e2ada4bfe643740f93b889ee30a53
wangtongmeng <tongmengwang@163.com>
2020-04-08 23:31:47 +0800

    css logo悬浮动画切换

---
+++ docs/.vuepress/components/logo.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="header-logo">
+    <a href="javascript:;"></a>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style>
+.header-logo {
+  display: inline-block;
+  width: 55px;
+  height: 55px;
+  background-color: #ff6600;
+}
+.header-logo a {
+  display: inline-block;
+  width: 110px;
+  height: 55px;
+}
+.header-logo a:before {
+  content: " ";
+  display: inline-block;
+  width: 55px;
+  height: 55px;
+  background: url("/assets/img/mi-logo.png") no-repeat center;
+  background-size: 55px;
+  transition: margin 0.2s;
+}
+.header-logo a:after {
+  content: " ";
+  display: inline-block;
+  width: 55px;
+  height: 55px;
+  background: url("/assets/img/mi-home.png") no-repeat center;
+  background-size: 55px;
+}
+.header-logo a:hover:before {
+  margin-left: -55px;
+  transition: margin 0.2s;
+}
+</style>
